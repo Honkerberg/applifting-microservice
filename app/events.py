9563c0578a0275620695210d8443be7ba9634e6e
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-07-10 22:23:47 +0200

    refactor: Refresh offers as variable
    We can set up different refresh time to get offers.

--- app/events.py
+++ app/events.py
@@ -1,7 +1,14 @@
+import os
+
 from app.background import fetch_offers
 from fastapi_utils.tasks import repeat_every
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REFRESH_OFFERS = os.getenv("OFFERS_REFRESH_TIME")
 
 
-@repeat_every(seconds=60)
+@repeat_every(seconds=int(REFRESH_OFFERS))
 async def fetch_and_create_offers():
     await fetch_offers()
